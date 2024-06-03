bf4ae45db004428a6bbc8c49810b1b69c712d80c
hachinrayhan <rayhan.hachin@gmail.com>
2024-06-04 02:31:35 +0600

    navbar and footer added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,77 @@
+import { Link } from "react-router-dom";
+// import useAuth from "../hooks/useAuth";
+
 const Navbar = () => {
+  //   const { user, logOut } = useAuth();
+  //   const handleLogout = async () => {
+  //     await logOut();
+  //   };
+  const navItems = (
+    <>
+      <li>
+        <Link to={"/"}>Home</Link>
+      </li>
+      <li>
+        <Link to={"/products"}>Products</Link>
+      </li>
+      <li>
+        <Link to={"/contacts"}>Contact Us</Link>
+      </li>
+      <li>
+        <Link to={"/dashboard"}>Dashboard</Link>
+      </li>
+      <li>
+        <button>Logout</button>
+      </li>
+      <li>
+        <Link to={"/login"}>Login</Link>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <h1>this is navbar</h1>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItems}
+          </ul>
+        </div>
+        <Link
+          to={"/"}
+          className="btn btn-ghost text-xl font-sans hover:font-serif"
+        >
+          POPPY&apos;S PREMIUM
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-lg menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <div className="avatar">
+          <div className="w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            {/* <img src={user?.photoURL} /> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
