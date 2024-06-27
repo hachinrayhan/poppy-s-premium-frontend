71199d244ded35d209c42b3856050bb17a01f379
hachinrayhan <rayhan.hachin@gmail.com>
2024-06-28 01:04:38 +0600

    loading spinner updated

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,9 @@
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-[calc(100vh-88px)]">
-      <p className="text-7xl font-bold">L</p>
-      <div className="w-12 h-12 border-8 border-dashed rounded-full animate-spin mt-5 border-red-500"></div>
-      <p className="text-7xl font-bold">ading....</p>
+      <p className="text-5xl md:text-7xl font-bold">L</p>
+      <div className="w-8 h-8 md:w-12 md:h-12 border-8 border-dashed rounded-full animate-spin mt-2 md:mt-5 border-red-500"></div>
+      <p className="text-5xl md:text-7xl font-bold">ading....</p>
     </div>
   );
 };
